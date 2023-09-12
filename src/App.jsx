5ef13410b597428f07b0a6e54a70ef27b029c276
Subroto Das <rupashi.bangla.club@gmail.com>
2023-09-12 16:39:46 +0600

    tailwind coustomization

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ function App() {
     <>
       <h1>Hellow I am React Component</h1>
       <p>Successfully Imported</p>
+      <button className="btn">
+        Home & Kitchen <span>^</span>
+      </button>
+      <button className="primary-btn">
+        Home & Kitchen <span>^</span>
+      </button>
     </>
   );
 }
