6298b2d3e9317a20bcdb954fa78724dc511491de
Subroto Das <rupashi.bangla.club@gmail.com>
2023-10-04 03:31:24 +0600

    Base64 Image

--- src/Components/AddUserByLogIn/AddUser.jsx
+++ src/Components/AddUserByLogIn/AddUser.jsx
@@ -179,6 +179,7 @@ const AddUser = () => {
               </label>
               <input
                 type="file"
+                name="image"
                 onChange={(e) => setImg(e.target.files[0])}
                 className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
               />
