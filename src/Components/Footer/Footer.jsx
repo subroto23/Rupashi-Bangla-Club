b63391448f018127aeacc4c26bd0a3a0c411762b
Subroto Das <rupashi.bangla.club@gmail.com>
2023-10-14 07:53:35 +0600

    Fotter Error Solve

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,15 +1,17 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => {
   const Year = new Date().getFullYear();
   return (
     <div className="mt-8">
       <footer className="bg-primary text-center text-white dark:bg-neutral-600 dark:text-neutral-200 lg:text-left">
-        <div className="flex border-b-2 border-neutral-200 dark:border-neutral-500 lg:justify-between py-4">
+        <div className="flex border-b-2 border-neutral-200 dark:border-neutral-500 max-w-6xl md:justify-between justify-center mx-auto py-4">
           <div className="mr-12 hidden lg:block">
             <span>আমাদের সাথে যুক্ত হোনঃ-</span>
           </div>
           {/* <!-- Social network icons container --> */}
           <div className="flex justify-center items-center">
-            <a className="mr-6 text-white dark:text-neutral-200">
+            <Link className="mr-6 text-white dark:text-neutral-200">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-4 md:h-4"
@@ -18,8 +20,8 @@ const Footer = () => {
               >
                 <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" />
               </svg>
-            </a>
-            <a className="mr-6 text-white dark:text-neutral-200">
+            </Link>
+            <Link className="mr-6 text-white dark:text-neutral-200">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-4 md:w-4"
@@ -28,8 +30,8 @@ const Footer = () => {
               >
                 <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
               </svg>
-            </a>
-            <a className="mr-6 text-white dark:text-neutral-200">
+            </Link>
+            <Link className="mr-6 text-white dark:text-neutral-200">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-5 md:w-5"
@@ -42,8 +44,8 @@ const Footer = () => {
                   clipRule="evenodd"
                 />
               </svg>
-            </a>
-            <a className="mr-6 text-white dark:text-neutral-200">
+            </Link>
+            <Link className="mr-6 text-white dark:text-neutral-200">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-4 md:w-4"
@@ -52,8 +54,8 @@ const Footer = () => {
               >
                 <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
               </svg>
-            </a>
-            <a className="mr-6 text-white dark:text-neutral-200">
+            </Link>
+            <Link className="mr-6 text-white dark:text-neutral-200">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-4 md:w-4"
@@ -62,8 +64,8 @@ const Footer = () => {
               >
                 <path d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z" />
               </svg>
-            </a>
-            <a className="text-white dark:text-neutral-200">
+            </Link>
+            <Link className="text-white dark:text-neutral-200">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-4 md:w-4 md:block hidden"
@@ -72,7 +74,7 @@ const Footer = () => {
               >
                 <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
               </svg>
-            </a>
+            </Link>
           </div>
         </div>
 
@@ -81,7 +83,7 @@ const Footer = () => {
           <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-4">
             {/* <!-- Tailwind Elements section --> */}
             <div className="md:block">
-              <h6 className="mb-4 flex items-center justify-center font-semibold uppercase md:justify-start">
+              <h6 className="mb-4 text-yellow-600 flex items-center justify-center font-semibold uppercase md:justify-start">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
@@ -92,55 +94,57 @@ const Footer = () => {
                 </svg>
                 রূপসী বাংলা ক্লাব
               </h6>
-              <p className="text-justify px-4">
+              {/* <p className="text-justify px-4">
                 আমরা সমাজের অসচ্ছল,হতদরিদ্র মানুষের সেবাই মূল লক্ষ্য।
-              </p>
+              </p> */}
             </div>
             {/* <!-- Products section --> */}
             <div className="grid grid-cols-2 md:pl-16 md:col-span-2">
               <div className="">
-                <h6 className="mb-4 flex justify-center font-semibold uppercase md:justify-start">
+                <h6 className="mb-4 text-yellow-600 flex justify-center font-semibold uppercase md:justify-start">
                   ভিজিট করুন
                 </h6>
                 <p className="mb-4">
-                  <a className="text-white dark:text-neutral-200">খবর</a>
+                  <Link className="text-white dark:text-neutral-200">খবর</Link>
                 </p>
                 <p className="mb-4">
-                  <a className="text-white dark:text-neutral-200">শিক্ষা</a>
+                  <Link className="text-white dark:text-neutral-200">শিক্ষা</Link>
                 </p>
                 <p className="mb-4">
-                  <a className="text-white dark:text-neutral-200">খেলাধুলা</a>
+                  <Link className="text-white dark:text-neutral-200">খেলাধুলা</Link>
                 </p>
                 <p>
-                  <a className="text-white dark:text-neutral-200">সেবাসমূহ</a>
+                  <Link className="text-white dark:text-neutral-200">সেবাসমূহ</Link>
                 </p>
               </div>
               {/* <!-- Useful links section --> */}
               <div className="">
-                <h6 className="mb-4 flex justify-center font-semibold uppercase md:justify-start">
+                <h6 className="mb-4 flex text-yellow-600 justify-center font-semibold uppercase md:justify-start">
                   আরো দেখুন
                 </h6>
                 <p className="mb-4">
-                  <a className="text-white dark:text-neutral-200">ছবি</a>
+                  <Link className="text-white dark:text-neutral-200">ছবি</Link>
                 </p>
                 <p className="mb-4">
-                  <a className="text-white dark:text-neutral-200">ভিডীও</a>
+                  <Link className="text-white dark:text-neutral-200">ভিডীও</Link>
                 </p>
                 <p className="mb-4">
-                  <a className="text-white dark:text-neutral-200">
+                  <Link className="text-white dark:text-neutral-200">
                     ক্লাব সদস্য
-                  </a>
+                  </Link>
                 </p>
                 <p>
-                  <a className="text-white dark:text-neutral-200">গ্রামবাসী</a>
+                  <Link className="text-white dark:text-neutral-200">গ্রামবাসী</Link>
                 </p>
               </div>
             </div>
             {/* <!-- Contact section --> */}
             <div className="md:block hidden">
-              <h6 className="mb-4 bg-[#5362ff] py-3 rounded-md flex justify-center font-semibold uppercase md:justify-start">
-                যোগাযোগ
-              </h6>
+              <div className="text-center">
+                <h6 className="mb-4 py-3 text-yellow-600 rounded-md flex justify-center font-semibold uppercase md:justify-start">
+                  যোগাযোগ
+                </h6>
+              </div>
               <p className="mb-4 flex items-center justify-center md:justify-start">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -187,12 +191,12 @@ const Footer = () => {
         {/* <!--Copyright section--> */}
         <div className="bg-blue-800 p-6 text-center dark:bg-neutral-700">
           <span>© {Year} কপিরাইট </span>
-          <a
+          <Link
             className="font-semibold text-white dark:text-neutral-400"
             href="https://rbcbd.vercel.app/"
           >
             রূপসী বাংলা ক্লাব
-          </a>
+          </Link>
         </div>
       </footer>
     </div>
