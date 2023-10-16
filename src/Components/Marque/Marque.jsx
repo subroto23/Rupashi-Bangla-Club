1ce6f2c4ec1bc4048b77b6014f2e02191150f162
Subroto Das <rupashi.bangla.club@gmail.com>
2023-10-17 00:18:48 +0600

    Marque Adding

---
+++ src/Components/Marque/Marque.jsx
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
+import Marquee from "react-fast-marquee";
+const Marque = () => {
+  const [titleLists, setTitleLists] = useState(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get("https://rbcwebsite.onrender.com/title/heading")
+      .then((res) => {
+        const titleValue = res.data.payload.data;
+        setTitleLists(titleValue.reverse().slice(0, -1)), setLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  return (
+    <div className="fixed bottom-0 z-10 bg-white text-gray-400 w-full">
+      {loading ? (
+        <span className="loading loading-spinner text-success"></span>
+      ) : (
+        <div className="py-1">
+          <Marquee>
+            {titleLists.map((data) => {
+              return (
+                <>
+                  <div className="mr-8">
+                    <span className="text-red-600 mx-1 font-bold animate-ping">
+                      <span className="loading loading-ring loading-sm"></span>
+                    </span>
+                    {data?.title}
+                  </div>
+                </>
+              );
+            })}{" "}
+          </Marquee>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Marque;
--- src/Components/Root/Root.jsx
+++ src/Components/Root/Root.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../../Layout/Header/Navbar";
 import Footer from "../Footer/Footer";
 import DateTimeHtml from "../../Services/DateTimeHtml";
+import Marque from "../Marque/Marque";
 
 const Root = () => {
   return (
@@ -10,6 +11,7 @@ const Root = () => {
       <Navbar />
       <Outlet />
       <Footer />
+      <Marque />
     </div>
   );
 };
