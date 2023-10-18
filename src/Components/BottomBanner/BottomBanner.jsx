0c608dabac8aab545663967eab7f1360bd7a4b91
Subroto Das <rupashi.bangla.club@gmail.com>
2023-10-18 15:41:17 +0600

    vercel

--- src/Components/BottomBanner/BottomBanner.jsx
+++ src/Components/BottomBanner/BottomBanner.jsx
@@ -19,8 +19,8 @@ const BottomBanner = () => {
         </div>
         <div data-aos="fade-left">
           <img
-            src="https://lh3.googleusercontent.com/pw/ADCreHch8OKNvZWzgOX0CT1nJ0LdB-9AP7njZpiqmisLp1kvo-Gaq5yD3aCG7fTg3VE0_Uf5sQVeiaPsSnbT6KYlsB5fjdTnQ6Wdf-ngeri5-3Sbu_DshxEShT7gUhlz1Y08CRcaLiv7XR81v2Qg_GRQe9oXySSwWhltlgQ3isbSCSL67wGSSKaxgjq6yr4W-iKp5QK1Mk9EbCHoFBSh0ovLLm2oElpYvMR6aXwGkbWWN9KqKuu7s_VLts7-v2pUv4eFAItNAWt3JGwb1zZnHKbm9fiNRO7pSRZUYGgzCQEOeFMAd6SY4ax_7aAZqa3NHLX4W7kp4xpjRmcLGTWSj6mzQU9UGW11vkB3GRfk6SRHu2g8Q1t_6qbIXAZyllnIBwE7vaT1n-ej6-9gRqTlJR5sER497TLwUQnF9R7U-1iQTkF5GIx947GAmIJlrZesXMV1ceutAT9l-fFHWe0xE-NDn6lyQxZ96viXyqzsUDzPvD5x1vmNyq4SKwUo19tYGmtekJ8b5nyYR91X0-8uowI_np6Dz18jUs4fEb_1ylpydwr8MoF9Ws3oaWugwZmMg2jOcsSXKekeTSAc0n77aLEWar19U0QdT5CKBF1O6N14ayNsy-d1DogacfvtbF3HdIXk0kV7UTqHf-7qc_jcEdJNncbYQ9qiJP7VV-86ZJn2bLzGvkSdcefUY-so7BHQ1wucM8IqMDiU4DtPQXjaCT_UAE35AJUmB-sn7ixCHsZJqVDtA8irBmnNflB7wodicUDTXzJwAikv7j9qM66bnSQivUYAnkFlXz6uZNyxCsIG6jpD3uayW2sILZtB0bRRIm5iuFWKB_4Vy4hgA2mDeZ0ygjYgwGND30U7G-3lp9NeqZyMVHVi-n2Thd3D-fh7FoynfM7tH2ATljbi0Pp2RsUvag=w1009-h931-s-no?authuser=0"
-            alt="3 women looking at a laptop"
+            src="https://i.ibb.co/0Js2j8Q/105629191-195857065094231-631298054909406055-n.jpg"
+            alt="RBC"
             className="object-cover w-full md:h-96 bg-gray-100 h-56"
             loading="lazy"
           />
