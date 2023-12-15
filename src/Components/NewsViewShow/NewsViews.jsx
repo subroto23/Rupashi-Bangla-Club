06451b8381983fb5e48c61973d7f0754735dcb56
Subroto Das <rupashi.bangla.club@gmail.com>
2023-12-15 11:09:37 +0600

    Helmet

--- src/Components/NewsViewShow/NewsViews.jsx
+++ src/Components/NewsViewShow/NewsViews.jsx
@@ -38,6 +38,8 @@ const NewsViews = () => {
           content={details}
           data-react-helmet="true"
         />
+        <meta name="description" content={details} />
+        <link rel="canonical" href={pageURL} />
         <meta
           property="og:image"
           content="https://media.istockphoto.com/id/1369150014/vector/breaking-news-with-world-map-background-vector.jpg?s=1024x1024&w=is&k=20&c=blBt3PJbOSEZF5_zB5YgKYeq9Zx_RMOLntX_nI3lliQ="
